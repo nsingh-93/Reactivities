ba08144beaa90fa63b7df793de89520a79551503
Neil <22299102+nsingh-93@users.noreply.github.com>
2026-02-19 20:25:04 -0500

    Seeding Data
    Added sample data to build the DB out of

--- API/Program.cs
+++ API/Program.cs
@@ -16,4 +16,20 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 app.MapControllers();
 
+// Service locator pattern
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+
+try
+{
+    var context = services.GetRequiredService<AppDbContext>();
+    await context.Database.MigrateAsync();
+    await DbInitializer.SeedData(context);
+}
+catch (Exception ex)
+{
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex, "Error occurred during migration");
+}
+
 app.Run();
--- Domain/obj/Debug/net10.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Debug/net10.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7275aac14b24bc07a0022c1a384a9ec26516989b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+202c4237e7af8dc493786cb171dae6b2d761b399")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
