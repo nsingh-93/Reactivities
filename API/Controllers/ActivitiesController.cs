28c8b8419aee4c41a3844e34f02262cff065c1b1
Neil <22299102+nsingh-93@users.noreply.github.com>
2026-02-27 01:00:22 -0500

    Started implementing CQRS
    Added queries for activities so far and working on commands to add, update, and delete next

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -1,26 +1,24 @@
 using System;
+using Application.Activities.Queries;
 using Domain;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
 
 namespace API.Controllers;
 
-public class ActivitiesController(AppDbContext context) : BaseApiController
+public class ActivitiesController(IMediator mediator) : BaseApiController
 {
     [HttpGet]
     public async Task<ActionResult<List<Activity>>> GetActivities()
     {
-        return await context.Activities.ToListAsync();
+        return await mediator.Send(new GetActivityList.Query());
     }
 
     [HttpGet("{id}")]
     public async Task<ActionResult<Activity>> GetActivityDetail(string id)
     {
-        var activity = await context.Activities.FindAsync(id);
-
-        if (activity == null) return BadRequest();
-
-        return activity;
+        return await mediator.Send(new GetActivityDetails.Query{Id = id});
     }
 }
