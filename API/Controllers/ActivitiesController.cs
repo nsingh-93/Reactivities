52af111a7a66f7acf9c8f523a4585893406df03c
Neil <22299102+nsingh-93@users.noreply.github.com>
2026-03-17 01:49:14 -0400

    Started adding Commands
    Added Commands folder and started with being able to add an activity

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -1,24 +1,28 @@
-using System;
+using Application.Activities.Commands;
 using Application.Activities.Queries;
 using Domain;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using Persistence;
 
 namespace API.Controllers;
 
-public class ActivitiesController(IMediator mediator) : BaseApiController
+public class ActivitiesController : BaseApiController
 {
     [HttpGet]
     public async Task<ActionResult<List<Activity>>> GetActivities()
     {
-        return await mediator.Send(new GetActivityList.Query());
+        return await Mediator.Send(new GetActivityList.Query());
     }
 
     [HttpGet("{id}")]
     public async Task<ActionResult<Activity>> GetActivityDetail(string id)
     {
-        return await mediator.Send(new GetActivityDetails.Query{Id = id});
+        return await Mediator.Send(new GetActivityDetails.Query{Id = id});
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<string>> CreateActivity(Activity activity)
+    {
+        return await Mediator.Send(new CreateActivity.Command{Activity = activity});
     }
 }
