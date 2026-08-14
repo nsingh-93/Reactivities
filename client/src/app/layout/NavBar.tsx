b500258ef6ae15b12afdbf03900896dfc3a3df55
Neil <22299102+nsingh-93@users.noreply.github.com>
2026-08-14 00:32:40 -0400

    Diving more into React project
    Added navbar and getting that set up while removing unneeded files and structuring folders better

---
+++ client/src/app/layout/NavBar.tsx
@@ -0,0 +1,28 @@
+// use 'rfc' to create React component like 'prop' for properties
+
+import { Menu } from "@mui/icons-material";
+import { Box, AppBar, Toolbar, IconButton, Typography, Button } from "@mui/material";
+
+export default function NavBar() {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <Menu />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            News
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  )
+}
