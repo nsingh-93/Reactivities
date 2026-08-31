c449bcd5997a4c206bb22f130a64d1a37b7e272e
Neil <22299102+nsingh-93@users.noreply.github.com>
2026-08-31 13:00:36 -0400

    Error handling added to API and client sides

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -17,28 +17,24 @@ public class ActivitiesController : BaseApiController
     [HttpGet("{id}")]
     public async Task<ActionResult<Activity>> GetActivityDetail(string id)
     {
-        return await Mediator.Send(new GetActivityDetails.Query{Id = id});
+        return HandleResult(await Mediator.Send(new GetActivityDetails.Query { Id = id }));
     }
 
     [HttpPost]
     public async Task<ActionResult<string>> CreateActivity(CreateActivityDto activityDto)
     {
-        return await Mediator.Send(new CreateActivity.Command{ActivityDto = activityDto});
+        return HandleResult(await Mediator.Send(new CreateActivity.Command { ActivityDto = activityDto }));
     }
 
     [HttpPut]
-    public async Task<ActionResult> EditActivity(Activity activity)
+    public async Task<ActionResult> EditActivity(EditActivityDto activityDto)
     {
-        await Mediator.Send(new EditActivity.Command{Activity = activity});
-        
-        return NoContent();
+        return HandleResult(await Mediator.Send(new EditActivity.Command { ActivityDto = activityDto }));
     }
 
     [HttpDelete("{id}")]
     public async Task<ActionResult> DeleteActivity(string id)
     {
-        await Mediator.Send(new DeleteActivity.Command{Id = id});
-
-        return Ok();
+        return HandleResult(await Mediator.Send(new DeleteActivity.Command { Id = id }));
     }
 }
---
+++ Application/Core/ValidationBehavior.cs
@@ -0,0 +1,23 @@
+using FluentValidation;
+using MediatR;
+
+namespace Application.Core;
+
+public class ValidationBehavior<TRequest, TResponse>(IValidator<TRequest>? validator = null)
+    : IPipelineBehavior<TRequest, TResponse> where TRequest : notnull
+{
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        if (validator == null) return await next();
+
+        var validationResults = await validator.ValidateAsync(request, cancellationToken);
+
+        if (!validationResults.IsValid)
+        {
+            throw new ValidationException(validationResults.Errors);
+        }
+
+        return await next();
+    }
+
+}
