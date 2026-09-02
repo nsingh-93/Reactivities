8612b112f57ed485fc74f89d14e5766061a81145
Neil <22299102+nsingh-93@users.noreply.github.com>
2026-09-02 14:37:51 -0400

    Added form validation on client side and map info for activities

--- client/src/features/activities/form/ActivityForm.tsx
+++ client/src/features/activities/form/ActivityForm.tsx
@@ -1,6 +1,6 @@
-import { Box, Button, Paper, TextField, Typography } from "@mui/material";
+import { Box, Button, Paper, Typography } from "@mui/material";
 import { useActivities } from "../../../lib/hooks/useActivities";
-import { useParams } from "react-router";
+import { useNavigate, useParams } from "react-router";
 import { useForm } from "react-hook-form";
 import { useEffect } from "react";
 import {
@@ -8,27 +8,55 @@ import {
   type ActivitySchema,
 } from "../../../lib/schemas/activitySchema";
 import { zodResolver } from "@hookform/resolvers/zod";
+import TextInput from "../../../app/shared/components/TextInput";
+import SelectInput from "../../../app/shared/components/SelectInput";
+import { categoryOptions } from "./categoryOptions";
+import DateTimeInput from "../../../app/shared/components/DateTimeInput";
+import LocationInput from "../../../app/shared/components/LocationInput";
 
 export default function ActivityForm() {
-  const {
-    register,
-    reset,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<ActivitySchema>({
+  const { control, reset, handleSubmit } = useForm<ActivitySchema>({
     mode: "onTouched",
     resolver: zodResolver(activitySchema),
   });
+  const navigate = useNavigate();
   const { id } = useParams();
   const { updateActivity, createActivity, activity, isLoadingActivity } =
     useActivities(id);
 
   useEffect(() => {
-    if (activity) reset(activity);
+    if (activity)
+      reset({
+        ...activity,
+        location: {
+          city: activity.city,
+          venue: activity.venue,
+          latitude: activity.latitude,
+          longitude: activity.longitude,
+        },
+      });
   }, [activity, reset]);
 
-  const onSubmit = (data: ActivitySchema) => {
-    console.log(data);
+  const onSubmit = async (data: ActivitySchema) => {
+    const { location, ...rest } = data;
+    const flattenedData = { ...rest, ...location };
+
+    try {
+      if (activity) {
+        updateActivity.mutate(
+          { ...activity, ...flattenedData },
+          {
+            onSuccess: () => navigate(`/activities/${activity.id}`),
+          },
+        );
+      } else {
+        createActivity.mutate(flattenedData, {
+          onSuccess: (id) => navigate(`/activities/${id}`),
+        });
+      }
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   if (isLoadingActivity) return <Typography>Loading activity...</Typography>;
@@ -45,45 +73,30 @@ export default function ActivityForm() {
         flexDirection="column"
         gap={3}
       >
-        <TextField
-          {...register("title")}
-          label="Title"
-          defaultValue={activity?.title}
-          error={!!errors.title}
-          helperText={errors.title?.message}
-        />
-        <TextField
-          {...register("description")}
+        <TextInput label="Title" control={control} name="title" />
+        <TextInput
           label="Description"
-          defaultValue={activity?.description}
+          control={control}
+          name="description"
           multiline
           rows={3}
         />
-        <TextField
-          {...register("category")}
-          label="Category"
-          defaultValue={activity?.category}
+        <Box display="flex" gap={3}>
+          <SelectInput
+            items={categoryOptions}
+            label="Category"
+            control={control}
+            name="category"
+          />
+          <DateTimeInput label="Date" control={control} name="date" />
+        </Box>
+
+        <LocationInput
+          control={control}
+          label="Enter the location"
+          name="location"
         />
-        <TextField
-          {...register("date")}
-          label="Date"
-          type="date"
-          defaultValue={
-            activity?.date
-              ? new Date(activity.date).toISOString().split("T")[0]
-              : new Date().toISOString().split("T")[0]
-          }
-        />
-        <TextField
-          {...register("city")}
-          label="City"
-          defaultValue={activity?.city}
-        />
-        <TextField
-          {...register("venue")}
-          label="Venue"
-          defaultValue={activity?.venue}
-        />
+
         <Box display="flex" justifyContent="end" gap={3}>
           <Button color="inherit">Cancel</Button>
           <Button
--- client/src/lib/types/index.d.ts
+++ client/src/lib/types/index.d.ts
@@ -6,7 +6,7 @@
 type Activity = {
   id: string;
   title: string;
-  date: string;
+  date: Date;
   description: string;
   category: string;
   isCancelled: boolean;
@@ -14,4 +14,34 @@ type Activity = {
   venue: string;
   latitude: number;
   longitude: number;
+};
+
+export type LocationIQSuggestion = {
+  place_id: string;
+  osm_id: string;
+  osm_type: string;
+  licence: string;
+  lat: string;
+  lon: string;
+  boundingbox: string[];
+  class: string;
+  type: string;
+  display_name: string;
+  display_place: string;
+  display_address: string;
+  address: LocationIQAddress;
+};
+
+export type LocationIQAddress = {
+  name: string;
+  road?: string;
+  neighbourhood?: string;
+  suburb?: string;
+  town?: string;
+  village?: string;
+  city?: string;
+  state: string;
+  postcode?: string;
+  country: string;
+  country_code: string;
 };
