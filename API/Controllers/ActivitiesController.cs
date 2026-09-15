32505c53bcdb16663957a8ad1cb362db6954a2b6
Neil <22299102+nsingh-93@users.noreply.github.com>
2026-09-14 22:44:09 -0400

    Updated the collection to be able to take teh bearer token return from login and to be used with policy in API

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -9,7 +9,6 @@ namespace API.Controllers;
 
 public class ActivitiesController : BaseApiController
 {
-    [AllowAnonymous]
     [HttpGet]
     public async Task<ActionResult<List<Activity>>> GetActivities()
     {
