26bc3e272c3d903f67f7739dce81c35a6716ac69
Neil <22299102+nsingh-93@users.noreply.github.com>
2026-09-16 16:59:32 -0400

    Added user menu and registration form as well as some error display on registration

--- client/src/lib/hooks/useAccount.ts
+++ client/src/lib/hooks/useAccount.ts
@@ -1,9 +1,13 @@
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import type { LoginSchema } from "../schemas/loginSchema";
 import agent from "../api/agent";
+import { useNavigate } from "react-router";
+import type { RegisterSchema } from "../schemas/registerSchema";
+import { toast } from "react-toastify";
 
 export const useAccount = () => {
   const queryClient = useQueryClient();
+  const navigate = useNavigate();
 
   const loginUser = useMutation({
     mutationFn: async (creds: LoginSchema) => {
@@ -16,16 +20,41 @@ export const useAccount = () => {
     },
   });
 
-  const { data: currentUser } = useQuery({
+  const registerUser = useMutation({
+    mutationFn: async (creds: RegisterSchema) => {
+      await agent.post("/account/register", creds);
+    },
+    onSuccess: () => {
+      toast.success("Registration successful");
+      navigate("/login");
+    },
+  });
+
+  const logoutUser = useMutation({
+    mutationFn: async () => {
+      await agent.post("/account/logout");
+    },
+    onSuccess: () => {
+      queryClient.removeQueries({ queryKey: ["user"] });
+      queryClient.removeQueries({ queryKey: ["activities"] });
+      navigate("/");
+    },
+  });
+
+  const { data: currentUser, isLoading: loadingUserInfo } = useQuery({
     queryKey: ["user"],
     queryFn: async () => {
       const response = await agent.get<User>("/account/user-info");
       return response.data;
     },
+    enabled: !queryClient.getQueryData(["user"]),
   });
 
   return {
     loginUser,
     currentUser,
+    logoutUser,
+    loadingUserInfo,
+    registerUser,
   };
 };
