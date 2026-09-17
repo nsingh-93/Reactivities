e131ebcde7c044abc33f790eb342eebf9f04dff6
Neil <22299102+nsingh-93@users.noreply.github.com>
2026-09-17 12:56:50 -0400

    Added Infrastructure and ability to have attendees for activities and updated mapping for new DTOs

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -10,13 +10,13 @@ namespace API.Controllers;
 public class ActivitiesController : BaseApiController
 {
     [HttpGet]
-    public async Task<ActionResult<List<Activity>>> GetActivities()
+    public async Task<ActionResult<List<ActivityDto>>> GetActivities()
     {
         return await Mediator.Send(new GetActivityList.Query());
     }
 
     [HttpGet("{id}")]
-    public async Task<ActionResult<Activity>> GetActivityDetail(string id)
+    public async Task<ActionResult<ActivityDto>> GetActivityDetail(string id)
     {
         return HandleResult(await Mediator.Send(new GetActivityDetails.Query { Id = id }));
     }
---
+++ Infrastructure/UserAccessor.cs
@@ -0,0 +1,23 @@
+using System.Security.Claims;
+using Application.Interfaces;
+using Domain;
+using Microsoft.AspNetCore.Http;
+using Persistence;
+
+namespace Infrastructure;
+
+public class UserAccessor(IHttpContextAccessor httpContextAccesor, AppDbContext dbContext) : IUserAccessor
+// IHttpContextAccessor contains the HttpContext which has the User object
+{
+    public async Task<User> GetUserAsync()
+    {
+        return await dbContext.Users.FindAsync(GetUserId())
+        ?? throw new UnauthorizedAccessException("No user is logged in");
+    }
+
+    public string GetUserId()
+    {
+        return httpContextAccesor.HttpContext?.User.FindFirstValue(ClaimTypes.NameIdentifier)
+            ?? throw new Exception("No user found");
+    }
+}
